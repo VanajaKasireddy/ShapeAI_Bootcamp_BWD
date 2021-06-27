e7b51d1c3fa0aef2fc30924d6885186864862756
VanajaKasireddy <86548251+VanajaKasireddy@users.noreply.github.com>
2021-06-27 12:45:55 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1 style={{ backgroundcolor: "crimson" }}>JAVASCRIPT AND REACT JS</h1>
+      <p style={{ backgroundcolor: "yellow" }}>
+        A basic web development React JS Bootcamp
+      </p>
+    </div>
+  );
+}
+
+export default Info;
